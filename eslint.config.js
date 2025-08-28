e28b98cca446b3b69131dde52debc9447638425d
Karel Švancar <karell.svancar@gmail.com>
2025-08-28 09:45:16 +0200

    Implement search logic

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "eslint-plugin-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+    plugins: {
+      prettier,
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // zapne Prettier přes ESLint
+      "prettier/prettier": "error",
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
     },
   },
-])
+]);
--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 import { Input } from "@karel/ctrl_f_pro_elements";
 
 function App() {
-    const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("");
 
+  //SDFfdsffds
+  // SFSDFFDF
 
-    return (
-        <div style={{ padding: "20px" }}>
-         TEst
-        </div>
-    );
+  return (
+    <div style={{ padding: "20px" }}>
+      TEst
+      <span> {search} </span>
+      <span> {search}</span>
+      <span> {search}</span>
+    </div>
+  );
 }
 
 export default App;
